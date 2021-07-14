5663aea1d65abea0eaacaa565f9340fcf44df3a9
saurav mitra <saurav.karate@gmail.com>
2021-07-15 01:00:52 +0800

    openVPN url

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ Configure AWS VPC access via OpenVPN using Terraform. This is part of the AWS de
 ## Resources
 - 1 Security Group
 - 1 EC2 Instance of OpenVPN Access Server
+
+## OpenVPN Setup
+- Browse to <openvpn_access_server_url> ignore SSL warning
+- Login with user as openvpn & <vpn_admin_password>
+- Download the VPN connection profile
+- Use OpenVPN client to connect to AWS VPC
--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@
 # Description: Outputs the OpenVPN Access Server IP
 # https://www.terraform.io/docs/configuration/outputs.html
 
-output "openvpn_access_server_ip" {
-  value       = aws_instance.openvpn_server.public_ip
-  description = "OpenVPN Access Server IP."
+output "openvpn_access_server_url" {
+  value       = "https://${aws_instance.openvpn_server.public_ip}:943/"
+  description = "OpenVPN Access Server URL."
 }
